2886b3ace085fcd248101a07d5123d49cadbcd07
jaechan <penguin_03@naver.com>
2024-12-02 16:25:01 +0900

    feat: user 모듈에 httpmodule, typeorm모듈 추가

--- backend-nest/src/users/users.module.ts
+++ backend-nest/src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [HttpModule, TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   controllers: [UsersController],
 })
