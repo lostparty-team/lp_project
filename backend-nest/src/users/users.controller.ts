0e7aa87a3df96c6c293cbbb43d24cd0984315ea0
jaechan <penguin_03@naver.com>
2024-12-02 16:24:22 +0900

    feat: user controller - register 엔드포인트 생성 및 작성

--- backend-nest/src/users/users.controller.ts
+++ backend-nest/src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RegisterUserRequestDto } from './dto/register-user.request.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  async register(@Body() registerUserRequestDto: RegisterUserRequestDto) {
+    await this.usersService.register(registerUserRequestDto);
+    return {
+      sucess: true,
+    };
+  }
+}
