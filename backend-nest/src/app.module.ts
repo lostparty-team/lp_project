6e0d65fa650e96332fb398ea161498acc38263ac
jaechan <penguin_03@naver.com>
2024-12-02 13:54:04 +0900

    feat: typeorm설정 추가 및 값 검증 추가

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -2,13 +2,33 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as session from 'express-session';
 import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
+        NODE_ENV: Joi.string().default('dev'),
         SESSION_SECRET: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().default(3306),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().optional().allow(''),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'], // 엔티티 경로 설정
+        synchronize: configService.get<string>('NODE_ENV') == 'dev', // 개발 중에만 사용 (주의)
       }),
     }),
   ],
