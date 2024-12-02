a7b3deb3e6807a7a4bcc037b3bac7c2c9546fcdd
jaechan <penguin_03@naver.com>
2024-12-02 13:06:26 +0900

    remove: 필요 없는 파일 삭제 - app.service.ts, app.controller.ts

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  controllers: [AppController], // 컨트롤러 등록
-  providers: [AppService], // 서비스 등록
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
