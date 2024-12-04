4afe128a7b56188e28a7c5aa85d098ef4b1aefb8
jaechan <penguin_03@naver.com>
2024-12-04 16:11:04 +0900

    refactor: user service 로그인 방식 수정 - login함수 리턴 값 수정

--- backend-nest/src/users/users.service.ts
+++ backend-nest/src/users/users.service.ts
@@ -1,7 +1,7 @@
+import { AuthService } from './../auth/auth.service';
 import { LoginUserRequestDto } from './dto/login-user.request.dto';
 import {
   BadRequestException,
-  HttpException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -18,6 +18,7 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
     private readonly httpService: HttpService,
+    private readonly authService: AuthService,
   ) {}
 
   async register(registerUserRequestDto: RegisterUserRequestDto) {
@@ -90,9 +91,11 @@ export class UsersService {
 
     if (!isMatch) throw new BadRequestException('정보를 다시 확인해주세요.');
 
-    const { password: pass, ...userInfowithoutPassword } = user;
+    const jwtPayload = { id: user.id };
 
-    return userInfowithoutPassword;
+    return {
+      access_token: await this.authService.generateToken(jwtPayload),
+    };
   }
 
   async findUser(id) {
