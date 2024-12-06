2a8df22accbc4eb233514d25e709eb263540f393
성인석 <inseok1999@gmail.com>
2024-12-06 16:52:39 +0900

    express 미들웨어 설정 , 도커컴포즈파일 설정 , db 초기화 sql 설정

--- backend-express/server.js
+++ backend-express/server.js
@@ -1,24 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-// JWT 검증 미들웨어
-function authenticateToken(req, res, next) {
-  const token = req.headers['authorization']?.split(' ')[1];
-  if (!token) {
-    return res.status(401).json({ error: '토큰이 제공되지 않았습니다.' });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).json({ error: '유효하지 않거나 만료된 토큰입니다.' });
-    }
-    req.user = user; // JWT의 payload 정보 저장
-    next();
-  });
-}
+// 별도로 분리된 미들웨어 가져오기
+const authenticateToken = require('./middleware/authenticateToken');
 
 app.use(express.json()); // POST 요청의 JSON 데이터 파싱
 
