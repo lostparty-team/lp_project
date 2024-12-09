ab72ad21833caba01b12ab99fe1343b5cdc957bc
Elbyss <elysiabyss@gmail.com>
2024-12-10 02:06:49 +0900

    Chore: 오타 수정

--- frontend/src/utils/header.ts
+++ frontend/src/utils/header.ts
@@ -1,7 +1,7 @@
 import axiosInstance from '@/api/axios';
 
 const setHeader = (key: string, value: string) => {
-  axiosInstance.defaults.headers.common[([key] = value)];
+  axiosInstance.defaults.headers.common[key] = value;
 };
 
 const removeHeader = (key: string) => {
