5944bb4884a68bc0692f9d6717d268262f1edcca
ehghks021203 <ehghks021203@gmail.com>
2024-12-11 04:03:35 +0900

    Feat: 블랙리스트 클릭 시 리스트를 볼 수 있는 모달 추가

--- frontend/src/app/blacklist/page.tsx
+++ frontend/src/app/blacklist/page.tsx
@@ -5,14 +5,15 @@ import { useRouter } from 'next/navigation';
 import { toast } from 'react-toastify';
 import '../../styles/pages/blacklist.css';
 import BlacklistCreateModal from '@/components/modal/blacklistCreate';
+import BlacklistModal from '@/components/modal/blacklistModal';
 
 interface BlacklistUser {
   id: number;
   name: string;
   createdBy: string;
   createdAt: string;
-  viewer: number;
-  rate: number;
+  add: number;
+  bad: number;
 }
 
 const BlacklistPage: React.FC = () => {
@@ -22,85 +23,81 @@ const BlacklistPage: React.FC = () => {
       name: '욕설 플레이어 모음',
       createdBy: '방울토마토라면',
       createdAt: '2024-11-25',
-      viewer: 1294,
-      rate: 153,
+      add: 1294,
+      bad: 153,
     },
     {
       id: 2,
       name: '숙코 모음',
       createdBy: '남양쮸강아지우',
       createdAt: '2024-11-28',
-      viewer: 16,
-      rate: 2,
+      add: 16,
+      bad: 2,
     },
     {
       id: 3,
       name: '비매너 플레이어',
       createdBy: '아기사슴설장군',
       createdAt: '2024-10-15',
-      viewer: 112,
-      rate: 33,
+      add: 112,
+      bad: 33,
     },
     {
       id: 4,
       name: '비매너 모음집',
       createdBy: '애니츠의겨울은너무추오',
       createdAt: '2024-11-26',
-      viewer: 133,
-      rate: 30,
+      add: 133,
+      bad: 30,
     },
     {
       id: 5,
       name: '내 블랙리스트',
       createdBy: '낟찔',
       createdAt: '2024-08-23',
-      viewer: 10,
-      rate: 1,
+      add: 10,
+      bad: 1,
     },
     {
       id: 6,
       name: '레이드 빌런',
       createdBy: 'HaeSungs',
       createdAt: '2024-09-19',
-      viewer: 242,
-      rate: 50,
+      add: 242,
+      bad: 50,
     },
     {
       id: 7,
       name: '레이드 빌런',
       createdBy: 'HaeSungs',
       createdAt: '2024-09-19',
-      viewer: 242,
-      rate: 50,
+      add: 242,
+      bad: 50,
     },
     {
       id: 8,
       name: '레이드 빌런',
       createdBy: 'HaeSungs',
       createdAt: '2024-09-19',
-      viewer: 242,
-      rate: 50,
+      add: 242,
+      bad: 50,
     },
     {
       id: 9,
       name: '레이드 빌런',
       createdBy: 'HaeSungs',
       createdAt: '2024-09-19',
-      viewer: 242,
-      rate: 50,
+      add: 242,
+      bad: 50,
     },
   ]);
 
   const [myBlacklist, setMyBlacklist] = useState<BlacklistUser[]>([]);
-  const [isBlacklistModalOpen, setBlacklistModalOpen] = useState<boolean>(false);
-  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+  const [listModalData, setListModalData] = useState<Record<string, any> | null>(null);
+  const [isCreateModalOpen, setCreateModalOpen] = useState<boolean>(false);
 
   const handleCreateBlacklist = () => {
-    setModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setModalOpen(false);
+    setCreateModalOpen(true);
   };
 
   const handleAddToMyBlacklist = (user: BlacklistUser) => {
@@ -121,7 +118,7 @@ const BlacklistPage: React.FC = () => {
       setBlacklist([...blacklist].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()));
     }
     if (event.target.value === 'popular') {
-      setBlacklist([...blacklist].sort((a, b) => b.rate - a.rate));
+      setBlacklist([...blacklist].sort((a, b) => b.bad - a.bad));
     }
   };
 
@@ -131,11 +128,12 @@ const BlacklistPage: React.FC = () => {
         <h1 className='mb-8 text-3xl font-bold text-lostark-400'>가장 인기 있는 블랙리스트</h1>
         <div className='grid grid-cols-3 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
           {blacklist
-            .sort((a, b) => b.rate - a.rate)
+            .sort((a, b) => b.bad - a.bad)
             .slice(0, 3)
             .map((data, index) => (
               <button
                 key={data.id}
+                onClick={() => setListModalData(data)}
                 className='relative overflow-hidden rounded-lg bg-gradient-to-br from-black2 to-black1 p-6 text-left shadow-lg transition-all duration-300 hover:scale-105'
               >
                 <span className='absolute right-4 top-4 rounded-full bg-orange-500 px-3 py-1 text-sm text-white'>
@@ -178,10 +176,10 @@ const BlacklistPage: React.FC = () => {
           >
             {blacklist.map((data) => (
               <li key={data.id} className='flex items-center justify-between rounded-lg bg-black2 p-4'>
-                <button className='w-full text-left'>
+                <button className='w-full text-left' onClick={() => setListModalData(data)}>
                   <p className='mb-2 text-lg font-semibold text-lostark-300'>{data.name}</p>
                   <p className='text-sm text-gray-400'>
-                    {data.createdBy} | {data.createdAt} | 담은수 {data.viewer} | 비추천 {data.rate}
+                    {data.createdBy} | {data.createdAt} | 담은수 {data.add} | 비추천 {data.bad}
                   </p>
                 </button>
                 <button
@@ -199,15 +197,15 @@ const BlacklistPage: React.FC = () => {
             <ul className='space-y-2 overflow-y-auto' style={{ height: 'calc(100vh - 420px)' }}>
               {myBlacklist.map((data) => (
                 <li key={data.id} className='flex items-center justify-between rounded-lg bg-black2 p-2'>
-                  <div>
+                  <button className='w-full text-left' onClick={() => setListModalData(data)}>
                     <p className='text-sm text-lostark-300'>{data.name}</p>
                     <p className='text-sm text-gray-400'>{data.createdBy}</p>
-                  </div>
+                  </button>
                   <button
                     onClick={() => handleRemoveFromMyBlacklist(data.id)}
-                    className='rounded-lg bg-red-500 px-2 py-1 text-black1 transition-all duration-300 hover:bg-red-400'
+                    className='w-[32px] rounded-lg bg-red-500 px-2 py-1 text-black1 transition-all duration-300 hover:bg-red-400'
                   >
-                    삭제
+                    -
                   </button>
                 </li>
               ))}
@@ -220,21 +218,8 @@ const BlacklistPage: React.FC = () => {
         <div className='mx-auto px-4 text-center text-white/50'>Copyright © All rights reserved.</div>
       </footer>
 
-      {isModalOpen && (
-        <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50'>
-          <div className='w-full max-w-3xl overflow-hidden rounded-lg bg-gray-800 shadow-lg'>
-            <div className='flex items-center justify-between bg-gray-700 p-4 text-white'>
-              <h2 className='text-xl font-bold'>블랙리스트 명단 생성</h2>
-              <button onClick={handleCloseModal} className='text-gray-300 transition duration-200 hover:text-white'>
-                ✕
-              </button>
-            </div>
-            <div className='p-4'>
-              <BlacklistCreateModal setModalOpen={setModalOpen} />
-            </div>
-          </div>
-        </div>
-      )}
+      {isCreateModalOpen && <BlacklistCreateModal setModalOpen={setCreateModalOpen} />}
+      {listModalData && <BlacklistModal data={listModalData} setModalData={setListModalData} />}
     </div>
   );
 };
