e1d250ea054d99ff571dcc684b4e054405eb49f0
성인석 <inseok1999@gmail.com>
2024-12-11 18:59:12 +0900

    이미지에서 텍스트 추출

--- backend-python/app.py
+++ backend-python/app.py
@@ -6,7 +6,6 @@ import pytesseract
 import io
 import os
 import json
-import base64
 
 from flask_cors import CORS
 app = Flask(__name__)
@@ -19,32 +18,21 @@ template_image = cv2.imread(template_path, cv2.IMREAD_COLOR)
 # 결과 이미지 저장 경로
 output_image_path = "masked_result.png"
 
-
 @app.route('/process', methods=['POST'])
 def process_image():
-    # POST 요청에서 Base64 인코딩된 이미지 읽기
-    data = request.json  # JSON 데이터 읽기
-    if 'image' not in data:
-        return jsonify({'error': 'No image data provided'}), 400
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file provided'}), 400
 
-    # Base64 디코딩
-    base64_image = data['image']
-    try:
-        image_data = base64.b64decode(base64_image)
-    except base64.binascii.Error:
-        return jsonify({'error': 'Invalid base64 encoding'}), 400
-
-    # OpenCV용 이미지 변환
-    np_image = np.frombuffer(image_data, np.uint8)
-    original_image = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
-    if original_image is None:
-        return jsonify({'error': 'Invalid image data'}), 400
+    # POST 요청에서 이미지 파일 읽기
+    file = request.files['image']
+    original_image = np.frombuffer(file.read(), np.uint8)
+    original_image = cv2.imdecode(original_image, cv2.IMREAD_COLOR)
 
     # 템플릿 매칭 수행
     result = cv2.matchTemplate(original_image, template_image, cv2.TM_CCOEFF_NORMED)
 
     # 임계값 설정 및 위치 찾기
-    threshold = data.get('threshold', 0.8)
+    threshold = 0.8
     locations = np.where(result >= threshold)
 
     # 템플릿 크기 및 사용자 지정 영역 설정
@@ -66,12 +54,15 @@ def process_image():
         mask[new_top_left[1]:new_bottom_right[1], new_top_left[0]:new_bottom_right[0]] = \
             original_image[new_top_left[1]:new_bottom_right[1], new_top_left[0]:new_bottom_right[0]]
 
+    # 결과 이미지를 파일로 저장
+    cv2.imwrite(output_image_path, mask)
+
     # OCR 처리
     pil_image = Image.fromarray(cv2.cvtColor(mask, cv2.COLOR_BGR2RGB))
     extracted_text = pytesseract.image_to_string(pil_image, lang="kor+eng", config="--oem 1 --psm 4")
     lines = [line.strip() for line in extracted_text.splitlines() if line.strip()]
 
-    # JSON 응답 반환
+    # 텍스트로 된 JSON 응답 반환
     response = {
         '닉네임': lines
     }
@@ -80,15 +71,6 @@ def process_image():
         status=200,
         mimetype='application/json'
     )
-
-
-# @app.route('/download_masked_image', methods=['GET'])
-# def download_masked_image():
-#     # 저장된 마스크 이미지를 반환
-#     if os.path.exists(output_image_path):
-#         return send_file(output_image_path, mimetype='image/png')
-#     else:
-#         return jsonify({'error': 'Masked image not found'}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
