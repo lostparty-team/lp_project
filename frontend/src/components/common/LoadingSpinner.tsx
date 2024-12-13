ad87a776a690942fae67226ec3806b942b920644
Elbyss <elysiabyss@gmail.com>
2024-12-14 02:16:12 +0900

    Style: 로딩스피너 색상 변경

--- frontend/src/components/common/LoadingSpinner.tsx
+++ frontend/src/components/common/LoadingSpinner.tsx
@@ -6,9 +6,9 @@ const LoadingSpinner = () => {
   if (!isLoading) return null;
 
   return (
-    <div className='absolute left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2'>
-      <div className='flex h-20 w-20 animate-spin items-center justify-center rounded-full border-4 border-transparent border-t-blue-400 text-4xl text-blue-400'>
-        <div className='flex h-16 w-16 animate-spin items-center justify-center rounded-full border-4 border-transparent border-t-red-400 text-2xl text-red-400'></div>
+    <div className='absolute left-1/2 top-1/2 z-[999] -translate-x-1/2 -translate-y-1/2'>
+      <div className='flex h-20 w-20 animate-spin items-center justify-center rounded-full border-4 border-transparent border-t-lostark-500 text-4xl text-lostark-500'>
+        <div className='flex h-16 w-16 animate-spin items-center justify-center rounded-full border-4 border-transparent border-t-lostark-300 text-2xl text-lostark-300'></div>
       </div>
     </div>
   );
