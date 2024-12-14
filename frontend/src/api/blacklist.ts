966a5fbebbd530524bddb3eb53c72e3882359b55
Elbyss <elysiabyss@gmail.com>
2024-12-14 23:58:02 +0900

    Chore(api): api 호출 오타 수정

--- frontend/src/api/blacklist.ts
+++ frontend/src/api/blacklist.ts
@@ -1,6 +1,5 @@
 import { axiosInstance } from './axios';
 import { BlacklistUser, SortType } from '@/types/blacklist';
-import axios from 'axios';
 
 export const blacklistApi = {
   getBlacklist: async (sortType?: SortType) => {
@@ -12,5 +11,5 @@ export const blacklistApi = {
 };
 
 export const getBlacklistDetail = (id: string) => {
-  return axios.get(`${process.env.NEXT_PUBLIC_TEST_API}/api/blacklist/${id}`);
+  return axiosInstance.get(`${process.env.NEXT_PUBLIC_TEST_API}/api/blacklist/${id}`);
 };
