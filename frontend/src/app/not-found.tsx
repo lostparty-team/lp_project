4ad2afe60d19b0652ebc94f0b2deaa5a9605a21a
Elbyss <elysiabyss@gmail.com>
2024-12-14 18:05:38 +0900

    Style: 404 page 스타일링

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,7 +1,43 @@
+'use client';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const NotFound = () => {
-  return <div className="flex min-h-screen items-center justify-center bg-gray-900 text-gray-100"></div>;
+  return (
+    <motion.div
+      transition={{ duration: 0.5 }}
+      className='flex h-[calc(100dvh-65px)] flex-col items-center justify-center gap-4 bg-zinc-900'
+    >
+      <motion.h1
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+        className='text-primary text-8xl font-bold text-red-500/70'
+      >
+        404
+      </motion.h1>
+      <motion.p
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.4, duration: 0.5 }}
+        className='text-2xl text-zinc-400'
+      >
+        페이지를 찾을 수 없습니다
+      </motion.p>
+      <motion.div
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+      >
+        <Link
+          href='/'
+          className='bg-primary hover:bg-primary/80 mt-4 rounded-lg px-6 py-2 text-sm font-semibold text-white/70 transition-colors hover:text-lostark-400 hover:transition-all'
+        >
+          홈으로 돌아가기
+        </Link>
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default NotFound;
