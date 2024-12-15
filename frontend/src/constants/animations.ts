24dfb6ca241951e82db6c52d2644fa4eafb4ba50
Elbyss <elysiabyss@gmail.com>
2024-12-16 02:41:15 +0900

    Chore(style): 페이지 공통 motion 추가

--- frontend/src/constants/animations.ts
+++ frontend/src/constants/animations.ts
@@ -1,3 +1,27 @@
+// export const pageVariants = {
+//   initial: {
+//     opacity: 0,
+//   },
+//   animate: {
+//     opacity: 1,
+//   },
+// };
+
+export const pageVariants = {
+  initial: {
+    opacity: 0,
+    x: 5,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.3,
+      ease: 'easeOut',
+    },
+  },
+};
+
 export const fadeInUp = {
   hidden: { opacity: 0, y: 20 },
   visible: (index: number = 0) => ({
