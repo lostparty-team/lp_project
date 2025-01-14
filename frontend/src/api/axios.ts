802ddd36b89d4de72b43fcccbc829354db7fe0c4
Elbyss <elysiabyss@gmail.com>
2025-01-14 22:50:06 +0900

    Chore: axios base url 변경

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_TEST_API,
+  baseURL: process.env.NEXT_PUBLIC_EXPRESS_API,
   withCredentials: true,
 });
 
