1563ba5085a2024c3f601014926c98ca2df8c4ed
Dohwan Kim <ehghks021203@gmail.com>
2025-02-12 21:28:56 +0900

    feat: 닉네임 수정 및 재요청 추가

--- frontend/src/api/process.ts
+++ frontend/src/api/process.ts
@@ -1,5 +1,4 @@
 import { AxiosResponse } from 'axios';
-import type { PartyScreen } from '../types/domain';
 import { axiosPythonInstance } from '@/api/axios';
 import { API } from '@/constants/route';
 
@@ -17,12 +16,12 @@ const authRequest = async (method: string, url: string, data?: any) => {
   });
 };
 
-export const getPartyScreenInfo = async (image: PartyScreen) => {
+export const getPartyScreenInfo = async (image: string) => {
   const { data } = await authRequest('post', API.PARTY.SCREEN, { image });
   return data;
 };
 
 export const getMemberInfo = async (nickname: string) => {
-  const {data} = await authRequest('get', `${API.PARTY.SCREEN}?nickname=${nickname}`);
+  const {data} = await authRequest('get', `${API.PARTY.MEMBER}?nickname=${nickname}`);
   return data;
 }
