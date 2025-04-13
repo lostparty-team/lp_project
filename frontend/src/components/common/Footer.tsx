dd3ae813632ac7d55f0d9e62e6920b1fdcef85c2
Elbyss <elysiabyss@gmail.com>
2025-04-13 23:49:19 +0900

    Chore: add footer link

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -1,12 +1,17 @@
+import { FOOTER_LINKS } from '@/constants/footer';
+import Link from 'next/link';
+
 export default function Footer() {
   return (
     <footer className='border-t border-lostark-400/10 bg-black1 py-16'>
       <div className='container mx-auto px-4'>
         <div className='mb-12 flex flex-wrap justify-center gap-8'>
-          {['문의하기', '후원하기'].map((item) => (
-            <button key={item} className='relative text-white/70 transition-all duration-300 hover:text-lostark-400'>
-              {item}
-            </button>
+          {FOOTER_LINKS.map(({ name, href }) => (
+            <Link href={href} target='blank'>
+              <button className='relative text-white/70 transition-all duration-300 hover:text-lostark-400'>
+                {name}
+              </button>
+            </Link>
           ))}
         </div>
         <p className='text-center text-white/50'>© {new Date().getFullYear()} 로스트파티. All rights reserved.</p>
