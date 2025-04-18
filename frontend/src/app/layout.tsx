562538609e22548280fc3cba1bafe4df5021394d
HaeSung99 <84333464+HaeSung99@users.noreply.github.com>
2025-04-19 08:26:17 +0900

    Google AdSense 스크립트 추가

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,8 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='ko' className='bg-black1'>
       <head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6105668975420119"
-     crossorigin="anonymous"></script>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6105668975420119" crossorigin="anonymous"></script>
       </head>
         <body className={pretendard.className}>
           <QueryProvider>
